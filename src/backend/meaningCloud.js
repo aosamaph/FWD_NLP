b6148ff26c86ee0f5b8663caa12f95bf7e011120
aosamaph <32076967+aosamaph@users.noreply.github.com>
2021-03-23 15:29:15 +0200

    update api key for review

--- src/backend/meaningCloud.js
+++ src/backend/meaningCloud.js
@@ -4,7 +4,8 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 const API_URL = 'https://api.meaningcloud.com/sentiment-2.1'
-const API_Key = process.env.API_KEY
+// const API_Key = process.env.API_KEY
+const API_Key = 'fe29625e15134d1d94eb61463a77dc9e'
 
 function AnalyzeArticle(link, lang = 'en') {
     // link = 'https://www.meaningcloud.com/developer/documentation'
